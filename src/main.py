531ed5e970f13a82a84f74554a79958641b97a77
yubb <2601169057@qq.com>
2017-11-15 16:54:26 +0800

    erro

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
-import surface.loginForm
-import sys
+# import surface.loginForm
+# import sys
+#
+# from PyQt5 import QtWidgets
 
-from PyQt5 import QtWidgets
+# if __name__ == '__main__':
+#     app = QtWidgets.QApplication(sys.argv)
+#     window = surface.loginForm.Ui_Form()
+#     window.show()
+#     sys.exit(app.exec_())
 
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    window = surface.loginForm.Ui_Form()
-    window.show()
-    sys.exit(app.exec_())
-
-
+from nltk.corpus import comtrans
+words = comtrans.words('alignment-en-fr.txt')
+for word in words:
+    print(word)
